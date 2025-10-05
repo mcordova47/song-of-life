9665abc0fa72af2cd0650421e4270a09c6ebf286
Marc Cordova <mcordova47@gmail.com>
2025-10-05 19:25:34 -0400

    working

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+export const playNotes_ = (notes, duration) => {
+  notes.forEach((note) => playNote(note, duration))
+}
+
+const ctx = new AudioContext()
+
+function playNote(freq, durationMs) {
+  const duration = durationMs / 1000
+  const osc = ctx.createOscillator()
+  const gain = ctx.createGain()
+  const attack = 0.01
+  const release = 0.05
+  const now = ctx.currentTime
+
+  osc.frequency.value = freq
+  osc.type = "sine"
+
+  // gain.gain.setValueAtTime(0, now);
+  // gain.gain.linearRampToValueAtTime(0.2, now + attack)
+  // gain.gain.setValueAtTime(0.2, now + duration - release)
+  // gain.gain.linearRampToValueAtTime(0, now + duration)
+
+  gain.gain.setValueAtTime(0.001, now)
+  gain.gain.exponentialRampToValueAtTime(0.2, now + attack)
+  gain.gain.setValueAtTime(0.2, now + duration - release)
+  gain.gain.exponentialRampToValueAtTime(0.001, now + duration)
+
+  osc.connect(gain).connect(ctx.destination)
+  osc.start(now)
+  osc.stop(now + duration)
+}
--- src/Main.purs
+++ src/Main.purs
@@ -4,23 +4,31 @@ import Prelude
 
 import Control.Alternative (guard)
 import Data.Array (foldl, (..))
+import Data.Array as Array
+import Data.Foldable (for_)
 import Data.Int as Int
+import Data.Maybe (Maybe(..), isJust)
 import Data.Set (Set)
 import Data.Set as Set
+import Data.Tuple (fst, snd)
 import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
 import Effect.Aff (Milliseconds(..), delay)
-import Elmish (Dispatch, ReactElement, Transition, fork, (<?|), (<|))
+import Effect.Class (liftEffect)
+import Effect.Uncurried (EffectFn2, runEffectFn2)
+import Elmish (Dispatch, ReactElement, Transition, forkVoid, forks, (<?|), (<|))
 import Elmish.Boot (defaultMain)
 import Elmish.HTML.Events as E
 import Elmish.HTML.Styled as H
+import Music (Note, a4, major2nd, major3rd, major6th, octave, perfect4th, perfect5th, (<<), (>>))
 
 main :: Effect Unit
 main = defaultMain { def: { init, view, update }, elementId: "app" }
 
 data Message
   = AutoStep
-  | Pause
+  | Beat (Array Note) Milliseconds
+  | Stop
   | Play
   | Reset
   | SetSpeed Int
@@ -29,7 +37,7 @@ data Message
 
 type State =
   { livingCells :: Set Cell
-  , play :: Boolean
+  , play :: Maybe Int
   , speed :: Int
   }
 
@@ -38,32 +46,54 @@ type Cell = Int /\ Int
 init :: Transition Message State
 init = pure
   { livingCells: Set.empty
-  , play: false
+  , play: Nothing
   , speed: 5
   }
 
 update :: State -> Message -> Transition Message State
 update state = case _ of
-  AutoStep | state.play -> do
-    fork do
-      delay $ Milliseconds $ 1000.0 / Int.toNumber state.speed
-      pure AutoStep
-    pure state { livingCells = step state.livingCells }
+  AutoStep | Just _ <- state.play -> do
+    let livingCells = step state.livingCells
+    autoStep livingCells
+    pure state { livingCells = livingCells, play = Just (Array.length (measure livingCells) - 1) }
   AutoStep ->
     pure state
-  Pause ->
-    pure state { play = false }
+  Beat notes' dur -> do
+    forkVoid $ liftEffect $ runEffectFn2 playNotes_ notes' dur
+    pure state { play = inc state.play }
+  Stop ->
+    pure state { play = Nothing }
   Play -> do
-    fork $ pure AutoStep
-    pure state { play = true }
+    autoStep state.livingCells
+    pure state { play = Just (-1) }
   Reset ->
-    pure state { livingCells = Set.empty :: _ Cell }
+    pure state { livingCells = Set.empty }
   SetSpeed speed ->
     pure state { speed = speed }
   Step ->
     pure state { livingCells = step state.livingCells }
   ToggleCell cell ->
     pure state { livingCells = Set.toggle cell state.livingCells }
+  where
+    autoStep cells =
+      forks \{ dispatch } -> do
+        let
+          measure' = measure cells
+          durationMs = duration / Int.toNumber state.speed / Int.toNumber (Array.length measure')
+        for_ measure' \notes' -> do
+          delay $ Milliseconds durationMs
+          liftEffect $ dispatch $ Beat notes' $ Milliseconds durationMs
+        liftEffect $ dispatch AutoStep
+
+    measure cells =
+      transpose grid
+      <#> Array.filter (\cell -> Set.member cell cells)
+      <#> map fst
+      <#> Array.mapMaybe (Array.index notes)
+
+    inc = case _ of
+      Just n -> Just $ mod (n + 1) (Array.length $ measure state.livingCells)
+      Nothing -> Nothing
 
 view :: State -> Dispatch Message -> ReactElement
 view state dispatch =
@@ -72,9 +102,9 @@ view state dispatch =
     [ gridView
     , H.div "mt-3"
       [ H.button_ "btn btn-primary"
-          { onClick: dispatch <| (if state.play then Pause else Play) }
-          if state.play then
-            "Pause"
+          { onClick: dispatch <| (if isJust state.play then Stop else Play) }
+          if isJust state.play then
+            "Stop"
           else
             "Play"
       , H.button_ "btn btn-outline-primary ms-2"
@@ -106,22 +136,60 @@ view state dispatch =
       grid <#> \row ->
         H.div "" $
           row <#> \cell ->
-            H.div_ ("d-inline-block bg-" <> if Set.member cell state.livingCells then "primary" else "light")
-              { style: H.css
-                  { width: 15
-                  , height: 15
-                  , cursor: "pointer"
-                  , margin: "0.1rem"
-                  }
-              , onClick: dispatch <| ToggleCell cell
-              }
-              H.empty
+            H.div_ "d-inline-block m-0 p-1"
+              { style: case state.play of
+                  Just n | n == snd cell -> H.css { backgroundColor: "#ffedf4" }
+                  _ -> H.css {}
+              } $
+              H.div_ ("d-inline-block bg-" <> if Set.member cell state.livingCells then "primary" else "light")
+                { style: H.css
+                    { width: 30
+                    , height: 30
+                    , cursor: "pointer"
+                    }
+                , onClick: dispatch <| ToggleCell cell
+                }
+                H.empty
 
 grid :: Array (Array Cell)
 grid =
-  cellRange <#> \row -> cellRange <#> \col -> row /\ col
+  rows <#> \row -> cols <#> \col -> row /\ col
   where
-    cellRange = 0 .. 29
+    rows = 0 .. Array.length notes
+    cols = 0 .. (beatsPerMeasure - 1)
+
+root :: Note
+root = a4 << octave
+
+notes :: Array Note
+notes = ((>>) root) <$>
+  [ mempty
+  , major3rd
+  , perfect5th
+  , major2nd
+  , perfect4th
+  , major6th
+  , octave
+  , octave <> major3rd
+  , octave <> perfect5th
+  , octave <> major2nd
+  , octave <> perfect4th
+  , octave <> major6th
+  ]
+
+duration :: Number
+duration = 15_000.0
+
+beatsPerMeasure :: Int
+beatsPerMeasure = 16
+
+transpose :: forall a. Array (Array a) -> Array (Array a)
+transpose rows = case Array.head rows of
+  Just row -> transpose' (Array.length row) rows
+  Nothing -> []
+  where
+    transpose' n = Array.replicate n [] # Array.foldl \cols row ->
+      Array.zipWith (<>) cols (row <#> Array.singleton)
 
 step :: Set Cell -> Set Cell
 step livingCells = foldl stepRow livingCells grid
@@ -146,3 +214,5 @@ step livingCells = foldl stepRow livingCells grid
       col' <- [col - 1, col, col + 1]
       guard $ (row' /\ col') /= (row /\ col)
       pure (row' /\ col')
+
+foreign import playNotes_ :: EffectFn2 (Array Note) Milliseconds Unit
