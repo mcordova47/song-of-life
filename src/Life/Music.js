f12fd0f1e398041250c1f11c266967823e6aa0b4
Marc Cordova <mcordova47@gmail.com>
2025-10-08 00:48:46 -0400

    normalize wave volumes

--- src/Life/Music.js
+++ src/Life/Music.js
@@ -7,14 +7,20 @@ export const playNote_ = (durationMs, wave, freq) => {
   const attack = 0.01
   const release = 0.05
   const now = ctx.currentTime
+  const minGain = 0.001
+  const maxGain = // ["square", "sawtooth"].includes(wave) ? 0.1 : 0.2
+    wave === "square" ? 0.08
+    : wave === "sawtooth" ? 0.13
+    : wave === "triangle" ? 0.24
+    : 0.25
 
   osc.frequency.value = freq
   osc.type = wave
 
-  gain.gain.setValueAtTime(0.001, now)
-  gain.gain.exponentialRampToValueAtTime(0.2, now + attack)
-  gain.gain.setValueAtTime(0.2, now + duration - release)
-  gain.gain.exponentialRampToValueAtTime(0.001, now + duration)
+  gain.gain.setValueAtTime(minGain, now)
+  gain.gain.exponentialRampToValueAtTime(maxGain, now + attack)
+  gain.gain.setValueAtTime(maxGain, now + duration - release)
+  gain.gain.exponentialRampToValueAtTime(minGain, now + duration)
 
   osc.connect(gain).connect(ctx.destination)
   osc.start(now)
