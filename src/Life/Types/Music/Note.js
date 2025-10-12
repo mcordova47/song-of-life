b02dbe03ee562427352a327f5a792d4c16dc1b6e
Marc Cordova <mcordova47@gmail.com>
2025-10-12 15:20:45 -0400

    pitch correct gain

--- src/Life/Types/Music/Note.js
+++ src/Life/Types/Music/Note.js
@@ -13,16 +13,25 @@ export const play_ = (durationMs, wave, freq) => {
     : wave === "sawtooth" ? 0.13
     : wave === "triangle" ? 0.24
     : 0.25
+  const pitchAdjustedGain = loudnessCompensation(freq) * maxGain
 
   osc.frequency.value = freq
   osc.type = wave
 
   gain.gain.setValueAtTime(minGain, now)
-  gain.gain.exponentialRampToValueAtTime(maxGain, now + attack)
-  gain.gain.setValueAtTime(maxGain, now + duration - release)
+  gain.gain.exponentialRampToValueAtTime(pitchAdjustedGain, now + attack)
+  gain.gain.setValueAtTime(pitchAdjustedGain, now + duration - release)
   gain.gain.exponentialRampToValueAtTime(minGain, now + duration)
 
   osc.connect(gain).connect(ctx.destination)
   osc.start(now)
   osc.stop(now + duration)
+}
+
+const loudnessCompensation = freq => {
+  const minFreq = 50
+  const maxFreq = 5000
+  const norm = Math.log(freq / minFreq) / Math.log(maxFreq / minFreq)
+  const dB = 6 * (1 - norm) - 3 * norm
+  return Math.pow(10, dB / 20)
 }
