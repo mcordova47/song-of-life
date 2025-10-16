9ef57a1b71ab000bc3b83c5ba0611992364958a6
Marc Cordova <mcordova47@gmail.com>
2025-10-15 22:47:13 -0400

    improve note quality

--- src/Life/Types/Music/Note.js
+++ src/Life/Types/Music/Note.js
@@ -1,4 +1,5 @@
-const ctx = typeof window === "undefined" ? null : new AudioContext()
+let ctx = null
+let reverb = null
 
 export const play_ = (durationMs, wave, freq) => {
   const duration = durationMs / 1000
@@ -14,16 +15,21 @@ export const play_ = (durationMs, wave, freq) => {
     : wave === "triangle" ? 0.24
     : 0.25
   const pitchAdjustedGain = loudnessCompensation(freq) * maxGain
+  const randRelease = Math.random() * release
+  const decay = release - randRelease
 
   osc.frequency.value = freq
   osc.type = wave
 
+  gain.connect(reverb.input.dry)
+  gain.connect(reverb.input.wetIn)
+
   gain.gain.setValueAtTime(minGain, now)
   gain.gain.exponentialRampToValueAtTime(pitchAdjustedGain, now + attack)
-  gain.gain.setValueAtTime(pitchAdjustedGain, now + duration - release)
-  gain.gain.exponentialRampToValueAtTime(minGain, now + duration)
+  gain.gain.setValueAtTime(pitchAdjustedGain, now + duration - randRelease)
+  gain.gain.exponentialRampToValueAtTime(minGain, now + duration + decay)
 
-  osc.connect(gain).connect(ctx.destination)
+  osc.connect(gain)
   osc.start(now)
   osc.stop(now + duration)
 }
@@ -34,4 +40,39 @@ const loudnessCompensation = freq => {
   const norm = Math.log(freq / minFreq) / Math.log(maxFreq / minFreq)
   const dB = 6 * (1 - norm) - 3 * norm
   return Math.pow(10, dB / 20)
+}
+
+const makeReverbChain = () => {
+  const delay = ctx.createDelay()
+  delay.delayTime.value = 0.1
+
+  const feedback = ctx.createGain()
+  feedback.gain.value = 0.2
+
+  const tone = ctx.createBiquadFilter()
+  tone.type = 'highpass'
+  tone.frequency.value = 400
+
+  const wet = ctx.createGain()
+  wet.gain.value = 0.15
+
+  const dry = ctx.createGain()
+  dry.gain.value = 0.7
+
+  delay.connect(feedback)
+  feedback.connect(tone)
+  tone.connect(delay)
+  delay.connect(wet)
+  wet.connect(ctx.destination)
+  dry.connect(ctx.destination)
+
+  return {
+    input: { dry, wetIn: tone },
+    delay, feedback, tone, wet, dry,
+  }
+}
+
+if (typeof window !== "undefined") {
+  ctx = new AudioContext()
+  reverb = makeReverbChain()
 }
